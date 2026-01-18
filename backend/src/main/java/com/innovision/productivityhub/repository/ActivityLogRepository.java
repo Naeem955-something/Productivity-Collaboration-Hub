ecdac27c3b8888217654ef7337faa3289241b413
Mohammad Naeem Mollah <nayemmollah165@gmail.com>
2026-01-19 00:10:58 +0600

    Update ActivityLogRepository.java

--- backend/src/main/java/com/innovision/productivityhub/repository/ActivityLogRepository.java
+++ backend/src/main/java/com/innovision/productivityhub/repository/ActivityLogRepository.java
@@ -1,6 +1,12 @@
 package com.innovision.productivityhub.repository;
 
 import com.innovision.productivityhub.model.ActivityLog;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> { }
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
+    List<ActivityLog> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    long deleteByCreatedAtBefore(java.time.Instant cutoff);
+}
