af49a963d92b09d22ac79a44a51f166e91fb5272
tonnii01 <toonnnii0049@gmail.com>
2026-01-25 01:00:50 +0600

    update Notifications.tsx

--- frontend/src/pages/Notifications.tsx
+++ frontend/src/pages/Notifications.tsx
@@ -1,4 +1,3 @@
-
 import { useMemo } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { notificationService, Notification } from '../services/dataService'
