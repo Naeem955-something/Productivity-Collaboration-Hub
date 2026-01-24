2fc9792d5ffb4d715bceb1faa5e15793492f388f
tonnii01 <toonnnii0049@gmail.com>
2026-01-25 00:42:43 +0600

    Add method to find FileItems by name

--- backend/src/main/java/com/innovision/productivityhub/repository/FileItemRepository.java
+++ backend/src/main/java/com/innovision/productivityhub/repository/FileItemRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileItemRepository extends JpaRepository<FileItem, Long> {
     List<FileItem> findByProjectId(Long projectId);
+    List<FileItem> findByNameContainingIgnoreCase(String name);
 }
