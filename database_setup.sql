f339758496c6eb981da12264b5680b2d5036f30f
akash209uiu <adas2310209@bscse.uiu.ac.bd>
2026-01-25 12:20:14 +0600

    Update in database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -127,7 +127,6 @@ CREATE TABLE IF NOT EXISTS notifications (
   FOREIGN KEY (recipient_id) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
 -- Documents table
 CREATE TABLE IF NOT EXISTS documents (
   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
